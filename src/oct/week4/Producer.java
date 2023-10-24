81ad8ce216effaba97a2af358e162cf33ddf48dd
prakashponali <p.ponali@gamil.com>
2023-10-24 12:01:27 +0530

    MulitThreading

---
+++ src/oct/week4/Producer.java
@@ -0,0 +1,34 @@
+package oct.week4;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author prakashponali
+ * @Date 24/10/23
+ */
+public class Producer implements Runnable {
+
+    public int threadId = 0;
+
+    public Lock lock;
+
+    public SharedResource sharedResource;
+
+    public Producer(int threadId, Lock lock, SharedResource sharedResource) {
+        this.threadId = threadId;
+        this.lock = lock;
+        this.sharedResource = sharedResource;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            lock.lock();
+            sharedResource.produce(1);
+            System.out.println("Producer " + threadId + " adding the inventory :: " + sharedResource.getInventory());
+        }finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ src/oct/week4/SharedResource.java
@@ -0,0 +1,26 @@
+package oct.week4;
+
+/**
+ * @author prakashponali
+ * @Date 24/10/23
+ */
+public class SharedResource {
+
+    public int inventory = 0;
+
+    public int getInventory(){
+
+        return inventory;
+    }
+
+    public void produce(int inventory) {
+        this.inventory = this.inventory + inventory;
+    }
+
+    public void consume(int inventory) {
+        if(inventory < this.inventory)
+            this.inventory = this.inventory - inventory;
+        else
+            this.inventory = 0;
+    }
+}
